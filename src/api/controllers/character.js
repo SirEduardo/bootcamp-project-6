e8b68c57f346e840797bd577dd9b237f93e194d0
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-06-17 11:16:39 +0200

    controllers update

--- src/api/controllers/character.js
+++ src/api/controllers/character.js
@@ -1,78 +1,94 @@
-
-const Character = require("../models/Character")
+const Character = require("../models/Character");
 
 const getCharacters = async (req, res, next) => {
-    try {
-        const characters = await Character.find().populate("movies")
-        return res.status(200).json(characters)
-    } catch (error) {
-        return res.status(404).json(error)
-    }
-}
+  try {
+    const characters = await Character.find().populate("movies");
+    return res.status(200).json(characters);
+  } catch (error) {
+    return res.status(404).json(error);
+  }
+};
 
-const getCharactersById = async (req, res,) => {
-    const id = req.params.id
-    try {
-        const character = await Character.findById(id).populate("movies")
-        if (character) {
-            return res.status(200).json(character)
-        }else{
-            return res.status(404).json("No character found")
-        }
-    } catch (error) {
-        return res.status(500).json(error)
+const getCharactersById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const character = await Character.findById(id).populate("movies");
+    if (character) {
+      return res.status(200).json(character);
+    } else {
+      return res.status(404).json("No character found");
     }
-}
-
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 
 const getCharactersByAlias = async (req, res) => {
-    const { alias } = req.params
-    try {
-        const characterByAlias = await Character.find({ alias: alias }).populate("movies")
-        return res.status(200).json(characterByAlias)
-    } catch (error) {
-        return res.status(500).json(error)
-    }
-}
-
+  const { alias } = req.params;
+  try {
+    const characterByAlias = await Character.find({ alias: alias }).populate(
+      "movies"
+    );
+    return res.status(200).json(characterByAlias);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 
 const postCharacter = async (req, res, next) => {
-    try {
-        const newCharacter = new Character(req.body)
-        const createdCharacter = await newCharacter.save()
-        return res.status(201).json(createdCharacter)
-    } catch (error) {
-        next(error)
+  try {
+    const newCharacter = new Character(req.body);
+
+    const characterDuplicated = await Character.findOne({ name: req.body.name })
+
+    if (characterDuplicated) {
+      return res.status(400).json("Este personaje ya existe")
     }
-}
+    const createdCharacter = await newCharacter.save();
+    return res.status(201).json(createdCharacter);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const editCharacter = async (req, res, next) => {
-    try {
-        const { id } = req.params
-        const characterModify = new Character(req.body)
-        characterModify._id = id
-        const characterUpdated = await Character.findByIdAndUpdate(id, characterModify, {new:true})
-        return res.status(200).json(characterUpdated)
-    } catch (error) {
-        return next(error)
-    }
-}
+  const { id } = req.params;
+  try {
+    const oldCharacter = await Character.find(id);
+    const updatedMovie = [
+      ...new set([...oldCharacter.movies, ...req.body.movies]),
+    ];
+
+    const updatedData = {
+      ...req.body,
+      movies: updatedMovie,
+    };
+    const characterUpdated = await Character.findByIdAndUpdate(
+      id,
+      updatedData,
+      { new: true }
+    );
+    return res.status(200).json(characterUpdated);
+  } catch (error) {
+    return next(error);
+  }
+};
 
 const deleteCharacter = async (req, res, next) => {
-    try {
-        const {id} = req.params
-        await Character.findByIdAndDelete(id)
-        return res.status(200).json("Character deleted!")
-    } catch (error) {
-        return next(error)
-    }
-}
+  try {
+    const { id } = req.params;
+    await Character.findByIdAndDelete(id);
+    return res.status(200).json("Character deleted!");
+  } catch (error) {
+    return next(error);
+  }
+};
 
 module.exports = {
-    getCharacters,
-    getCharactersById,
-    getCharactersByAlias,
-    postCharacter,
-    editCharacter,
-    deleteCharacter,
-}
+  getCharacters,
+  getCharactersById,
+  getCharactersByAlias,
+  postCharacter,
+  editCharacter,
+  deleteCharacter,
+};
--- src/api/controllers/movies.js
+++ src/api/controllers/movies.js
@@ -1,54 +1,64 @@
-const Movies = require("../models/movies")
-
-
-
+const Movies = require("../models/movies");
 
 const getMovies = async (req, res, next) => {
-    try {
-        const movies = await  Movies.find().populate("characters")
-        return res.status(200).json(movies)
-    } catch (error) {
-        return res.status(400).json(error)
-    }
-}
+  try {
+    const movies = await Movies.find().populate("characters");
+    return res.status(200).json(movies);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
 
 const postMovie = async (req, res, next) => {
-    try {
-        const newMovie = new Movies(req.body)
-        const createdMovie = await newMovie.save()
-        return res.status(200).json(createdMovie)
-    } catch (error) {
-        return res.status(400).json(error)
+  try {
+    const newMovie = new Movies(req.body);
+
+    const movieDuplicated = await Movies.findOne({ name: req.body.name })
+
+    if (movieDuplicated) {
+      return res.status(400).json("Esta pelicula ya existe")
     }
-}
+
+    const createdMovie = await newMovie.save();
+    return res.status(200).json(createdMovie);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
 
 const updateMovie = async (req, res, next) => {
-    try {
-        const { id } = req.params
-        const oldMovie = await Movies.findById(id)
-        newMovie = new Movies(req.body)
-        newMovie._id = id
-        newMovie.characters = [...oldMovie.characters, ...req.body.characters]
-        const updatedMovie = await Movies.findByIdAndUpdate(id, newMovie, {new:true})
-        return res.status(200).json(updatedMovie)
-    } catch (error) {
-        return res.status(400).json(error)
-    }
-}
+  const { id } = req.params;
+  try {
+    const oldMovie = await Movies.findById(id);
+    const updatedCharacters = [
+      ...new set([...oldMovie.characters, ...req.body.characters]),
+    ];
+    const updatedData = {
+      ...req.body,
+      characters: updatedCharacters,
+    };
+    const updatedMovie = await Movies.findByIdAndUpdate(id, updatedData, {
+      new: true,
+    });
+    return res.status(200).json(updatedMovie);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
 
 const deleteMovie = async (req, res, next) => {
-    try {
-        const { id } = req.params
-        await Movies.findByIdAndDelete(id)
-        return res.status(200).json(deletedMovie)
-    } catch (error) {
-        return res.status(400).json(error)
-    }
-}
+  try {
+    const { id } = req.params;
+    await Movies.findByIdAndDelete(id);
+    return res.status(200).json(deletedMovie);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
 
 module.exports = {
-    getMovies,
-    postMovie,
-    updateMovie,
-    deleteMovie,
-}
+  getMovies,
+  postMovie,
+  updateMovie,
+  deleteMovie,
+};
